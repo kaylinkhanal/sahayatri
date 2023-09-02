8a73157072720a65a1bed7ea67dfd2beddb0aa82
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-09-02 13:00:54 +0545

    Implemented focusing of phone number field on first mount

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,11 @@
-import UserMenu from '@/components/UserMenu'
-import React, {useEffect, useState} from 'react'
-import { GoogleMap, useJsApiLoader,MarkerF } from '@react-google-maps/api'
-import styles from '../../styles/userMenu.module.css'
-import CheckIcon from '@mui/icons-material/Check';
-import MapSearch from '../../components/MapSearch'
-import { DotWave } from '@uiball/loaders'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import UserMenu from "@/components/UserMenu";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import styles from "../../styles/userMenu.module.css";
+import CheckIcon from "@mui/icons-material/Check";
+import MapSearch from "../../components/MapSearch";
+import { DotWave } from "@uiball/loaders";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 function index() {
   const [searchStep, setSearchStep] = useState(1);
   const { isLoaded, loadError } = useJsApiLoader({
@@ -31,12 +31,29 @@ function index() {
     lat: 27.702348,
     lng: 85.307631,
   });
+
+  // Just playing around with static data
+  const dest = {
+    lat: 26.4175616,
+    lng: 87.2677376,
+  };
+
+  // Finding averages
+  const centerLat = (center.lat + dest.lat) / 2;
+  const centerLng = (center.lng + dest.lng) / 2;
+
+  const centerLatLng = {
+    lat: centerLat,
+    lng: centerLng,
+  };
+
   // useEffect(() => {
   //   navigator.geolocation.getCurrentPosition((pos)=> {
   //     const {latitude, longitude} = pos.coords
   //     setCenter({lat:latitude, lng:longitude})
   //   });
   // }, [])
+
   const mapContainerStyle = {
     height: "100vh",
     width: "100vw",
@@ -46,61 +63,61 @@ function index() {
     return (
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        center={center}
-          zoom={13}
-    >
-      <MarkerF
-      onLoad={()=> console.log("loaded")}
-      position={center}
-      draggable={true}
-      icon={{
-        // path: google.maps.SymbolPath.CIRCLE,
-        
-        url:'/pickuplocation.png'
-      }}
-    />
+        center={centerLatLng}
+        zoom={9}
+      >
+        <MarkerF
+          onLoad={() => console.log("loaded")}
+          position={center}
+          draggable={true}
+          icon={{
+            // path: google.maps.SymbolPath.CIRCLE,
+
+            url: "/pickuplocation.png",
+          }}
+        />
         <MarkerF
           onLoad={() => console.log("loaded")}
           icon={"/destination-marker.svg"}
           position={center}
           draggable={true}
         />
- 		<div className={styles.map}>
-					{searchStep === 1 && (
-						<div className="flex items-center gap-1">
-							<MapSearch showCurrentIcon={true} placeholder="Pickup Address" />
+        <div className={styles.map}>
+          {searchStep === 1 && (
+            <div className="flex items-center gap-1">
+              <MapSearch showCurrentIcon={true} placeholder="Pickup Address" />
 
-							<CheckIcon
-								className={styles.custom}
-								onClick={() => setSearchStep(2)}
-							/>
-						</div>
-					)}
-					{searchStep === 2 && (
-						<div className="flex items-center gap-1">
-							<MapSearch
-								showCurrentIcon={false}
-								placeholder="Destination Address"
-							/>
-							<ArrowBackIcon
-								className={styles.custom}
-								onClick={() => setSearchStep(1)}
-							/>
-						</div>
-					)}
-				</div>
-    {/* <div className={styles.map}>
+              <CheckIcon
+                className={styles.custom}
+                onClick={() => setSearchStep(2)}
+              />
+            </div>
+          )}
+          {searchStep === 2 && (
+            <div className="flex items-center gap-1">
+              <MapSearch
+                showCurrentIcon={false}
+                placeholder="Destination Address"
+              />
+              <ArrowBackIcon
+                className={styles.custom}
+                onClick={() => setSearchStep(1)}
+              />
+            </div>
+          )}
+        </div>
+        {/* <div className={styles.map}>
 
     </div> */}
       </GoogleMap>
     );
   }
 
- return (
-  <div className='flex justify-center items-center h-screen'>
-    <DotWave size={70} speed={1} color="#2563eb" />
-  </div>
- )
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <DotWave size={70} speed={1} color="#2563eb" />
+    </div>
+  );
 }
 
 export default index;
